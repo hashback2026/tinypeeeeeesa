a8f15adb5cd92c2ff2b5e359ae9af1805dea456e
hashback2026 <dstvsasapay5@gmail.com>
2026-05-08 21:55:14 +0300

    Update tinypesa.js

--- utils/tinypesa.js
+++ utils/tinypesa.js
@@ -6,7 +6,7 @@ async function stkPush(phone, amount, transactionCode = "BulkPay") {
 
     try {
 
-        // Normalize phone number
+        // Normalize phone
         let msisdn = phone
             .replace(/\s+/g, "")
             .replace(/[^0-9]/g, "");
@@ -19,25 +19,33 @@ async function stkPush(phone, amount, transactionCode = "BulkPay") {
 
         const payload = {
             amount: Number(amount),
-            msisdn: msisdn,
+            msisdn,
             account_no: transactionCode,
             callback_url: process.env.TINYPESA_WEBHOOK_URL
         };
 
-        const response = await axios.post(
-            "https://api.tinypesa.com/api/v1/express/initialize",
-            payload,
-            {
-                headers: {
-                    "ApiKey": API_KEY,
-                    "Content-Type": "application/json"
-                }
-            }
-        );
+        console.log("Sending payload:", payload);
+
+        const response = await axios({
+            method: "post",
+            url: "https://api.tinypesa.com/api/v1/express/initialize/",
+            headers: {
+                "ApiKey": API_KEY,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            data: payload,
+            maxRedirects: 0
+        });
 
         return response.data;
 
     } catch (error) {
+
+        console.log(
+            "TinyPesa Error:",
+            error.response?.data || error.message
+        );
 
         return {
             error: true,
